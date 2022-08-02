82d561b8fde24da7b42ac8634fb6aad243ca48b9
Anuska86 <anasrm86@gmail.com>
2022-08-02 16:31:22 +0200

    ToDos

--- ToDoList/main.js
+++ ToDoList/main.js
@@ -1,6 +1,25 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem("todos")) || [];
+const render = () => {
+  const toDoList = document.getElementById("todoList");
+  const todosTemplate = todos.map((t) => "<li>" + t + "</li>");
+  toDoList.innerHTML = todosTemplate.join("");
+  const elements = document.querySelectorAll("#todoList li");
+  elements.forEach((element1, i) => {
+    element1.addEventListener("click", () => {
+      element1.parentNode.removeChild(element1);
+      todos.splice(i, 1);
+      updateTodos(todos);
+      render();
+    });
+  });
+};
 
+const updateTodos = () => {
+  const todoStrings = JSON.stringify(todos);
+  localStorage.setItem("todos", todoStrings);
+};
 window.onload = () => {
+  render();
   const form = document.getElementById("todoForm");
   form.onsubmit = (e) => {
     e.preventDefault();
@@ -8,12 +27,6 @@ window.onload = () => {
     const todoText = todo.value;
     todo.value = "";
     todos.push(todoText);
-    const toDoList = document.getElementById("todoList");
-    const todosTemplate = todos.map((t) => "<li>" + t + "</li>");
-    toDoList.innerHTML = todosTemplate.join("");
-    const elements = document.querySelectorAll("#todoList li");
-    elements.forEach((element1, i) => {
-      console.log(element1, i);
-    });
+    updateTodos(todos);
   };
 };
